449f1a67835642251b00e5ebf477f1bbe125db0c
Muzammil Ali <muzammilali5994@outlook.com>
2026-03-01 16:59:23 +0500

    practise

--- index.js
+++ index.js
@@ -169,3 +169,93 @@ addDataButton.addEventListener("click", addData);
       console.log("Document does not exist");
     }
   }
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  
+  import { getFirestore, doc, setDoc,   collection, updateDoc , getDoc } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+  import { getAuth,createUserWithEmailAndPassword ,onAuthStateChanged ,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+  
+
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyDjIzFxciCfd5PuWoYIPst6nVVY4Q1S9LQ",
+  authDomain: "class-practise-e3470.firebaseapp.com",
+  projectId: "class-practise-e3470",
+  storageBucket: "class-practise-e3470.firebasestorage.app",
+  messagingSenderId: "842044276464",
+  appId: "1:842044276464:web:862e23813224fd7df1cc35",
+  measurementId: "G-C79FZGKR8J"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+
+
+//Sign Up 
+
+  const signUpEmail = document.getElementById("semail");
+  const signUpPassword = document.getElementById("spassword");
+  const signUpButton = document.querySelector("button");
+
+  signUpButton.addEventListener("click", signup);
+
+  function signup() {
+      createUserWithEmailAndPassword(auth, signUpEmail.value, signUpPassword.value)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+  }
+
+
+// Sign In
+
+  const signin = document.getElementById("signin");
+  const email = document.getElementById("Iemail");
+  const password = document.getElementById("Ipassword");
+  signin.addEventListener("click", signinf);
+
+
+  function signinf() {
+    signInWithEmailAndPassword(auth, email.value, password.value )
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  
+  }
+
+  // add data
+
+  const dname = document.getElementById("dname");
+  const dage = document.getElementById("dage");
+  const addDataButton = document.getElementById("addDataButton");
+  addDataButton.addEventListener("click", addData);
+
+  function addData() {
+    setDoc(doc(db, "users", "user2"), {
+      name: dname.value,
+      age: dage.value 
+    })
+    .then(() => {
+      console.log("Data added");
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("Not added");
+    });
+  }
